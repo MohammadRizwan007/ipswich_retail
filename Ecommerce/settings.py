1d684af8e3502ad1379bd02bb8d1d6741904933f
MohammadRizwan007 <mmohammad.rizwan@gmail.com>
2025-09-17 00:52:58 +0100

    updated docker

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -355,9 +355,9 @@ LOGGING = {
 }
 
 # Security settings for production
-if not DEBUG:
-    SECURE_SSL_REDIRECT = True
-    SESSION_COOKIE_SECURE = True
-    CSRF_COOKIE_SECURE = True
-    SECURE_BROWSER_XSS_FILTER = True
-    SECURE_CONTENT_TYPE_NOSNIFF = True
+# if not DEBUG:
+#     SECURE_SSL_REDIRECT = True
+#     SESSION_COOKIE_SECURE = True
+#     CSRF_COOKIE_SECURE = True
+#     SECURE_BROWSER_XSS_FILTER = True
+#     SECURE_CONTENT_TYPE_NOSNIFF = True
